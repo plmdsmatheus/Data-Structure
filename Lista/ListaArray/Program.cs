91d48e65cef75a716f3f9a09e761b5cf6fe3af53
Matheus Duarte <matheusduartenatal@gmail.com>
2023-10-05 19:21:59 -0300

    TAD List with array and Doubly linked list

---
+++ Lista/ListaArray/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+public class Lista<T>
+{
+    private T[] array;
+    private int size;
+    private const int DefaultCapacity = 10;
+
+    public Lista()
+    {
+        array = new T[DefaultCapacity];
+        size = 0;
+    }
+
+    public int Size()
+    {
+        return size;
+    }
+
+    public bool IsEmpty()
+    {
+        return size == 0;
+    }
+
+    public T First()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("A lista está vazia.");
+        }
+        return array[0];
+    }
+
+    public T Last()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("A lista está vazia.");
+        }
+        return array[size - 1];
+    }
+
+    public T Before(int p)
+    {
+        if (p <= 0 || p >= size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(p));
+        }
+        return array[p - 1];
+    }
+
+    public T After(int p)
+    {
+        if (p < 0 || p >= size - 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(p));
+        }
+        return array[p + 1];
+    }
+
+    public void ReplaceElement(int n, T o)
+    {
+        if (n < 0 || n >= size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+        array[n] = o;
+    }
+
+    public void SwapElements(int n, int q)
+    {
+        if (n < 0 || n >= size || q < 0 || q >= size)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T temp = array[n];
+        array[n] = array[q];
+        array[q] = temp;
+    }
+
+    public void InsertBefore(int n, T o)
+    {
+        if (n < 0 || n >= size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        for (int i = size; i > n; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[n] = o;
+        size++;
+    }
+
+    public void InsertAfter(int n, T o)
+    {
+        if (n < 0 || n >= size - 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        for (int i = size - 1; i > n; i--)
+        {
+            array[i + 1] = array[i];
+        }
+
+        array[n + 1] = o;
+        size++;
+    }
+
+    public void InsertFirst(T o)
+    {
+        InsertBefore(0, o);
+    }
+
+    public void InsertLast(T o)
+    {
+        if (size >= array.Length)
+        {
+            EnsureCapacity(size + 1);
+        }
+
+        array[size] = o;
+        size++;
+    }
+
+    public void Remove(int n)
+    {
+        if (n < 0 || n >= size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        for (int i = n; i < size - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+
+        size--;
+    }
+
+    private void EnsureCapacity(int CapacidadeMin)
+    {
+        int novaCapacidade = array.Length == 0 ? DefaultCapacity : array.Length * 2;
+        if (novaCapacidade < CapacidadeMin)
+        {
+            novaCapacidade = CapacidadeMin;
+        }
+        Array.Resize(ref array, novaCapacidade);
+    }
+}
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        Lista<int> lista = new Lista<int>();
+
+        Console.WriteLine("Size: " + lista.Size());
+        Console.WriteLine("Is the list empty? " + lista.IsEmpty());
+
+        lista.InsertLast(1);
+        lista.InsertLast(2);
+        lista.InsertLast(3);
+
+        Console.WriteLine("Size: " + lista.Size());
+        Console.WriteLine("First object: " + lista.First());
+        Console.WriteLine("Last object: " + lista.Last());
+
+        Console.WriteLine("Object before index 2: " + lista.Before(2));
+        Console.WriteLine("Object after index 0: " + lista.After(0));
+
+        lista.ReplaceElement(1, 10);
+        Console.WriteLine("Replace object at index 1 with 10: " + lista.Last());
+
+        lista.SwapElements(0, 2);
+        Console.WriteLine("Swap objects at indexes 0 and 2: First object: " + lista.First() + ", Last object: " + lista.Last());
+
+        lista.InsertBefore(1, 5);
+        Console.WriteLine("Inserted object 5 before index 1: First object: " + lista.First() + ", Size: " + lista.Size());
+
+        lista.InsertAfter(2, 8);
+        Console.WriteLine("Inserted oboject 8 after index 2: Last object: " + lista.Last() + ", List size: " + lista.Size());
+
+        lista.InsertFirst(0);
+        Console.WriteLine("Inserted object 0 at the beginning of the list: First object: " + lista.First() + ", Size: " + lista.Size());
+
+        lista.Remove(3);
+        Console.WriteLine("Removed element at index 3: List size: " + lista.Size());
+
+    }
+}
---
+++ Lista/ListaEncadeado/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+
+public class Node<T>
+{
+    public T Element { get; set; }
+    public Node<T> Next { get; set; }
+    public Node<T> Previous { get; set; }
+
+    public Node(T element)
+    {
+        Element = element;
+        Next = null;
+        Previous = null;
+    }
+}
+
+public class DoublyLinkedList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    private int count;
+
+    public DoublyLinkedList()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public int Size()
+    {
+        return count;
+    }
+
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    public bool IsFirst(Node<T> node)
+    {
+        return node == head;
+    }
+
+    public bool IsLast(Node<T> node)
+    {
+        return node == tail;
+    }
+
+    public Node<T> First()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("A lista está vazia.");
+        return head;
+    }
+
+    public Node<T> Last()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("A lista está vazia.");
+        return tail;
+    }
+
+    public Node<T> Before(Node<T> node)
+    {
+        if (node.Previous == null)
+            throw new InvalidOperationException("O nó não tem um nó anterior.");
+        return node.Previous;
+    }
+
+    public Node<T> After(Node<T> node)
+    {
+        if (node.Next == null)
+        {
+            throw new InvalidOperationException("O nó não tem um proximo nó .");
+        }
+        return node.Next;
+    }
+
+    public void ReplaceElement(Node<T> node, T element)
+    {
+        node.Element = element;
+    }
+
+    public void SwapElements(Node<T> node1, Node<T> node2)
+    {
+        T temp = node1.Element;
+        node1.Element = node2.Element;
+        node2.Element = temp;
+    }
+
+    public void InsertBefore(Node<T> node, T element)
+    {
+        Node<T> newNode = new Node<T>(element);
+        newNode.Previous = node.Previous;
+        newNode.Next = node;
+        if (node.Previous == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            node.Previous.Next = newNode;
+        }
+        node.Previous = newNode;
+        count++;
+    }
+
+    public void InsertAfter(Node<T> node, T element)
+    {
+        Node<T> newNode = new Node<T>(element);
+        newNode.Previous = node;
+        newNode.Next = node.Next;
+        if (node.Next == null)
+        {
+            tail = newNode;
+        }
+        else
+        {
+            node.Next.Previous = newNode;
+        }
+        node.Next = newNode;
+        count++;
+    }
+
+    public void InsertFirst(T element)
+    {
+        Node<T> newNode = new Node<T>(element);
+        newNode.Next = head;
+        if (head == null)
+        {
+            tail = newNode;
+        }
+        else
+        {
+            head.Previous = newNode;
+        }
+        head = newNode;
+        count++;
+    }
+
+    public void InsertLast(T element)
+    {
+        Node<T> newNode = new Node<T>(element);
+        newNode.Previous = tail;
+        if (tail == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+        }
+        tail = newNode;
+        count++;
+    }
+
+    public void Remove(Node<T> node)
+    {
+        if (node.Previous == null)
+        {
+            head = node.Next;
+        }
+        else
+        {
+            node.Previous.Next = node.Next;
+        }
+
+        if (node.Next == null)
+        {
+            tail = node.Previous;
+        }
+        else
+        {
+            node.Next.Previous = node.Previous;
+        }
+
+        count--;
+    }
+}
+class Test
+{
+    static void Main(string[] args)
+    {
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+        Console.WriteLine("Empty list? " + list.IsEmpty());
+        Console.WriteLine("Size: " + list.Size());
+
+        list.InsertFirst(1);
+        list.InsertLast(2);
+        list.InsertAfter(list.First(), 3);
+        list.InsertBefore(list.Last(), 4);
+
+        Console.WriteLine("is empty list? " + list.IsEmpty());
+        Console.WriteLine("Size: " + list.Size());
+
+        Console.WriteLine("First object " + list.First().Element);
+        Console.WriteLine("Last object: " + list.Last().Element);
+
+        Console.WriteLine("Object before last: " + list.Before(list.Last()).Element);
+        Console.WriteLine("Object after first: " + list.After(list.First()).Element);
+
+        Console.WriteLine("List objects:");
+        Node<int> currentNode = list.First();
+        while (currentNode != null)
+        {
+            Console.WriteLine(currentNode.Element);
+            currentNode = list.After(currentNode);
+        }
+
+        list.Remove(list.First());
+
+        Console.WriteLine("Object in the list after the first one is removed:");
+        currentNode = list.First();
+        while (currentNode != null)
+        {
+            Console.WriteLine(currentNode.Element);
+            currentNode = list.After(currentNode);
+        }
+    }
+}
